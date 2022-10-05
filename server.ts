014ebdb72f503ad0d7e98b0f322e91e3926daf9a
fredrick <yalleyfred@gmail.com>
2022-10-05 11:18:40 +0000

    added app.ts and database.ts

---
+++ server.ts
@@ -0,0 +1,19 @@
+import {app} from "./app"
+import dotenv from 'dotenv';
+import sequelize from './Database';
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+
+sequelize.authenticate().then(() => {
+    console.log('connected to database successfully!'); 
+  }).catch(err => {
+    console.log('DB connection failed');
+    
+  })
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  });
