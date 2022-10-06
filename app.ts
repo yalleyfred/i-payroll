a0a25cdfaa66b69001b9ba75ac42d88285e467b0
devBoison <enochboison@amalitech.org>
2022-10-06 18:50:39 +0000

    update folder structure

--- app.ts
+++ app.ts
@@ -1,11 +1,23 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
 
 dotenv.config();
 
 export const app: Express = express();
 
+import employeeRoute from './routes/employeeRoute';
+
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use('/api/v1/employees', employeeRoute);
